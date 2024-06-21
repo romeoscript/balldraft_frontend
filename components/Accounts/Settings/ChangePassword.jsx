2313a373d8a11c26c1ea83ee8e18bbbb0e4e3a86
romeoscript <romeobourne211@gmail.com>
2024-06-21 14:05:35 +0100

    done

--- components/Accounts/Settings/ChangePassword.jsx
+++ components/Accounts/Settings/ChangePassword.jsx
@@ -1,174 +1,196 @@
-// ProfileForm.js
 "use client";
 
-import React, { useState } from 'react';
-import { Formik, Form, Field, useField } from 'formik';
+import React, { useState } from "react";
+import { Formik, Form, Field, useField } from "formik";
 import {
-    TextField,
-    Button,
-    FormControl,
-    FormHelperText,
-    IconButton,
-    InputAdornment,
-} from '@mui/material';
-import LockIcon from '@mui/icons-material/Lock';
-import styled from '@emotion/styled';
-import * as Yup from 'yup';
-import { Checkbox, FormControlLabel } from '@mui/material';
+  TextField,
+  Button,
+  FormControl,
+  FormHelperText,
+  InputAdornment,
+  IconButton,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import LockIcon from "@mui/icons-material/Lock";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import styled from "@emotion/styled";
+import * as Yup from "yup";
+import axios from "axios";
+import { Toaster, toast } from "react-hot-toast";
 
 const MuiFormikCheckbox = ({ label, ...props }) => {
-    const [field] = useField(props);
-    return (
-        <FormControlLabel
-            className="my-[0.5rem]"
-            control={<Checkbox {...field} checked={field.value} color="primary" />}
-            label={label}
-        />
-    );
+  const [field] = useField(props);
+  return (
+    <FormControlLabel
+      className="my-[0.5rem]"
+      control={<Checkbox {...field} checked={field.value} color="primary" />}
+      label={label}
+    />
+  );
 };
 
 const StyledTextField = styled(TextField)(({ error }) => ({
-    ...(error && {
-        '& .MuiOutlinedInput-root': {
-            '& fieldset': {
-                borderColor: 'red',
-            },
-            '&:hover fieldset': {
-                borderColor: 'red',
-            },
-            '&.Mui-focused fieldset': {
-                borderColor: 'red',
-            },
-        },
-    }),
+  ...(error && {
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "red",
+      },
+      "&:hover fieldset": {
+        borderColor: "red",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "red",
+      },
+    },
+  }),
 }));
 
-const MuiFormikField = ({ field, form: { touched, errors }, icon, ...props }) => (
-    <FormControl fullWidth error={touched[field.name] && !!errors[field.name]}>
-        <StyledTextField
-            {...field}
-            {...props}
-            error={touched[field.name] && !!errors[field.name]}
-            InputProps={{
-                startAdornment: (
-                    <InputAdornment position="start">
-                        {icon}
-                    </InputAdornment>
-                ),
-            }}
-        />
-        {touched[field.name] && errors[field.name] && (
-            <FormHelperText>{errors[field.name]}</FormHelperText>
-        )}
-    </FormControl>
+const MuiFormikField = ({
+  field,
+  form: { touched, errors },
+  icon,
+  type,
+  showPassword,
+  toggleShowPassword,
+  ...props
+}) => (
+  <FormControl fullWidth error={touched[field.name] && !!errors[field.name]}>
+    <StyledTextField
+      {...field}
+      {...props}
+      type={showPassword ? "text" : type}
+      error={touched[field.name] && !!errors[field.name]}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">{icon}</InputAdornment>
+        ),
+        endAdornment: (
+          <InputAdornment position="end">
+            <IconButton
+              aria-label={`toggle ${field.name} visibility`}
+              onClick={toggleShowPassword}
+              edge="end"
+            >
+              {showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+    />
+    {touched[field.name] && errors[field.name] && (
+      <FormHelperText>{errors[field.name]}</FormHelperText>
+    )}
+  </FormControl>
 );
 
 const ChangePasswordSchema = Yup.object().shape({
-    currentPassword: Yup.string().required("Current password is required"),
-    newPassword: Yup.string().when('showNewPassword', {
-        is: true,
-        then: Yup.string().required("New password is required")
-    }),
-    confirmPassword: Yup.string().when('showNewPassword', {
-        is: true,
-        then: Yup.string()
-            .required("Please confirm your new password")
-            .oneOf([Yup.ref('newPassword'), null], "Passwords must match")
-    }),
+  newPassword: Yup.string().required("New password is required"),
+  confirmPassword: Yup.string()
+    .required("Please confirm your new password")
+    .oneOf([Yup.ref("newPassword"), null], "Passwords must match"),
 });
 
-const Changepassword = () => {
-    const [showNewPassword, setShowNewPassword] = useState(false);
+const ChangePassword = () => {
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-    const initialValues = {
-        currentPassword: '',
-        newPassword: '',
-        confirmPassword: '',
-        rememberMe: false,
-        showNewPassword: false
-    };
+  const toggleShowNewPassword = () => {
+    setShowNewPassword(!showNewPassword);
+  };
 
-    const onSubmit = (values) => {
-        console.log(values);
-        // Handle form submission
-    };
+  const toggleShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
 
-    return (
-        <div className="p-6 rounded-lg w-[70%]">
-            <Formik
-                initialValues={initialValues}
-                validationSchema={ChangePasswordSchema}
-                onSubmit={onSubmit}
+  const initialValues = {
+    newPassword: "",
+    confirmPassword: "",
+    rememberMe: false,
+  };
+
+  const onSubmit = async (values, { setSubmitting }) => {
+    const url = process.env.NEXT_PUBLIC_API_URL;
+    try {
+      const response = await axios.patch(
+        `${url}set-new-password/`,
+        {
+          password: values.newPassword,
+          confirm_password: values.confirmPassword,
+        //   token: 1,
+        //   uidb64: 1
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${sessionStorage.getItem("access_token")}`,
+          },
+        }
+      );
+      toast.success("Password updated successfully");
+    } catch (error) {
+      console.error("Error updating password:", error);
+      toast.error("Failed to update password");
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <div className="p-6 rounded-lg w-full m-auto">
+      <Toaster />
+      <Formik
+        initialValues={initialValues}
+        validationSchema={ChangePasswordSchema}
+        onSubmit={onSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div className="text-center my-[1rem]">
+              <h2 className="font-bold text-2xl">Change Your Password</h2>
+              <p>
+                To enhance your security, please update your password below.
+              </p>
+            </div>
+            <Field
+              name="newPassword"
+              component={MuiFormikField}
+              label="New Password"
+              type="password"
+              variant="outlined"
+              margin="normal"
+              icon={<LockIcon />}
+              showPassword={showNewPassword}
+              toggleShowPassword={toggleShowNewPassword}
+            />
+            <Field
+              name="confirmPassword"
+              component={MuiFormikField}
+              label="Confirm Password"
+              type="password"
+              variant="outlined"
+              margin="normal"
+              icon={<LockIcon />}
+              showPassword={showConfirmPassword}
+              toggleShowPassword={toggleShowConfirmPassword}
+            />
+            <MuiFormikCheckbox
+              name="rememberMe"
+              label="By clicking “Save change” you confirm that you are the account holder and authorize the password update."
+            />
+            <Button
+              type="submit"
+              className="bg-[#012C51] w-full text-white p-[1rem] hover:bg-[#4096FF] rounded-[30px]"
+              disabled={isSubmitting}
             >
-                {({ isSubmitting, setFieldValue }) => (
-                    <Form>
-                        <div className="text-center my-[1rem]">
-                            <h2 className="font-bold text-2xl">Change Your Password</h2>
-                            <p>To enhance your security, please update your password below.</p>
-                        </div>
-
-                        {!showNewPassword && (
-                            <>
-                                <Field
-                                    name="currentPassword"
-                                    component={MuiFormikField}
-                                    label="Current Password"
-                                    type="password"
-                                    variant="outlined"
-                                    margin="normal"
-                                    icon={<LockIcon />}
-                                />
-                                <Button
-                                    type="button"
-                                    className="bg-[#012C51] w-full text-white p-[1rem] hover:bg-[#4096FF] rounded-[30px]"
-                                    onClick={() => {
-                                        setShowNewPassword(true);
-                                        setFieldValue("showNewPassword", true);
-                                    }}
-                                >
-                                    Continue
-                                </Button>
-                            </>
-                        )}
-
-                        {showNewPassword && (
-                            <>
-                                <Field
-                                    name="newPassword"
-                                    component={MuiFormikField}
-                                    label="New Password"
-                                    type="password"
-                                    variant="outlined"
-                                    margin="normal"
-                                    icon={<LockIcon />}
-                                />
-                                <Field
-                                    name="confirmPassword"
-                                    component={MuiFormikField}
-                                    label="Confirm Password"
-                                    type="password"
-                                    variant="outlined"
-                                    margin="normal"
-                                    icon={<LockIcon />}
-                                />
-                                <MuiFormikCheckbox
-                                    name="rememberMe"
-                                    label="By clicking “Save change” you confirm that you are the account holder and authorize the Email Address update."
-                                />
-                                <Button
-                                    type="submit"
-                                    className="bg-[#012C51] w-full text-white p-[1rem] hover:bg-[#4096FF] rounded-[30px]"
-                                    disabled={isSubmitting}
-                                >
-                                    Save Changes
-                                </Button>
-                            </>
-                        )}
-                    </Form>
-                )}
-            </Formik>
-        </div>
-    );
+              Save Changes
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
 
-export default Changepassword;
+export default ChangePassword;
